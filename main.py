45b8fbd799c448d5eb2d021d359458563f4e6402
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2021-11-01 15:04:16 +0300

    Start
    Start

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import pymysql
 import PyQt5
 import pandas
 import sys
+import os
+
+r"""
+command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+"""
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 or sys.argv[1] == "console":
+
 
     else:
         # TODO GUI
