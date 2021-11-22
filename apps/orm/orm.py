3167567f6df301f4d143f760e1dcaafce836ec6e
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2021-11-22 17:55:00 +0300

    Update orm.py

--- apps/orm/orm.py
+++ apps/orm/orm.py
@@ -1,8 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGridLayout
-from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QTableWidget
@@ -10,20 +8,29 @@ from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QListWidget
-from PyQt5.QtWidgets import QListWidgetItem
-from db.db import Database
 from sqlalchemy import create_engine
 
+
+from .university_objects import *
+import inspect
 
 
 class ORMApplication(QtWidgets.QMainWindow):
 
-    def __init__(self):
+    def __init__(self, echo=True):
         super().__init__()
 
         # model part
-        self.engine = create_engine("mysql+pymysql://root:password@localhost:3306/university", echo=True, future=True)
+        self.engine = create_engine("mysql+pymysql://root:password@localhost:3306/university", echo=echo, future=True)
+        self.session = sessionmaker(bind=self.engine)()
 
+        self.tables = ["Department", "Professor", "StudyGroup", "Student",
+                       "TheorySubject", "Audience", "Grade", "Class"]
+
+        self.current = -1
+
+        self.keys = dict()
+        self.objects = []
         # UI part
 
         self.setWindowTitle("University GUI")
@@ -43,19 +50,27 @@ class ORMApplication(QtWidgets.QMainWindow):
 
         self.entities = QComboBox()
         self.entities.setFixedWidth(150)
-        self.filter = QComboBox()
-        self.filter.setFixedWidth(150)
-        self.objects = QListWidget()
-        self.objects.setFixedWidth(150)
+
+        for table in self.tables:
+            self.entities.addItem(table)
+
+        self.filters = QComboBox()
+        self.filters.setFixedWidth(150)
+
+        self.list = QListWidget()
+        self.list.setFixedWidth(150)
 
         self.layout.addWidget(self.entities, QtCore.Qt.AlignTop)
-        self.layout.addWidget(self.filter, QtCore.Qt.AlignTop)
-        self.layout.addWidget(self.objects, QtCore.Qt.AlignTop)
+        self.layout.addWidget(self.filters, QtCore.Qt.AlignTop)
+        self.layout.addWidget(self.list, QtCore.Qt.AlignTop)
 
         # signals
-        #self.objects.clicked.connect(self.element_clicked)
+        self.list.clicked.connect(self.element_clicked)
+        self.entities.textActivated.connect(self.text_selected)
+        self.filters.textActivated.connect(self.text_selected)
 
         self.general_layout.addLayout(self.layout)
+        self.update_left()
 
     def _init_right(self):
 
@@ -65,6 +80,28 @@ class ORMApplication(QtWidgets.QMainWindow):
         # self.table.cellChanged.connect(self.changed_item)
 
         self.general_layout.addWidget(self.table)
+
+    def update_list(self):
+
+        current = self.entities.currentText()
+        self.list.clear()
+        self.keys = dict()
+        self.objects = []
+        filter = self.filters.currentText()
+        for index, instance in enumerate(self.session.query(OrmFactory(current))):
+            self.list.addItem(str(instance.id) + ": " + str(getattr(instance, filter)))
+            self.objects.append(instance)
+            self.keys[instance.id] = index
+
+    def update_left(self):
+
+        self.filters.clear()
+        current = self.entities.currentText()
+        attributes = class_to_columns(current)
+        for a in attributes:
+            self.filters.addItem(a)
+
+        self.update_list()
 
     def make_request(self, button_name):
 
@@ -132,11 +169,45 @@ class ORMApplication(QtWidgets.QMainWindow):
     #         except ...:
     #             ORMApplication.error("Error", "Error", "Error")
 
+    def text_selected(self, s):
+
+        if s in self.tables:
+            self.update_left()
+        else:
+            self.update_list()
+
     def element_clicked(self, item):
+
+        id = int(item.data().split(": ")[0])
+        obj = self.objects[self.keys[id]]
+
+        result = {}
+        # disconnect from table
+        #self.table.cellChanged.disconnect(self.element_clicked)
+
+        self.table.setColumnCount(0)
+        self.table.setRowCount(0)
+
+        #columns = ["Атрибуты", "Значения"]
+        attributes = ["id"] + class_to_columns(self.entities.currentText())
         self.table.setColumnCount(1)
-        self.table.setRowCount(1)
-        self.table.setItem(0, 0, QTableWidgetItem(str(25)))
-        print('click -> {}'.format(item.data()))
+        self.table.setRowCount(len(attributes))
+
+        #self.table.setHorizontalHeaderLabels()
+        self.table.setVerticalHeaderLabels(attributes)
+        #for i in range(0, len(columns)):
+        #    self.table.horizontalHeaderItem(i).setTextAlignment(Qt.AlignLeft)
+
+        for i, row in enumerate(attributes):
+            for j, col in enumerate(["Значение"]):
+                self.table.setItem(i, j, QTableWidgetItem(str(getattr(obj, attributes[i]))))
+
+        self.table.resizeColumnsToContents()
+
+        # reconnect to table
+        #self.table.cellChanged.connect(self.element_clicked)
+
+
 
     @staticmethod
     def error(text: str = "", info_text: str = "", title: str = ""):
